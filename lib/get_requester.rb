4e836ca2bff07fa0b86408bca252ced69db88ee0
brody sullivan <brodysullivan0@gmail.com>
2020-07-01 01:40:05 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,5 +2,9 @@ class GetRequester
   
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
-  
+  def parse_json
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
 end
